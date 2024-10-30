035094be7dff080fe6e1df7aa72d9919df77f900
bendoza77 <gabrieljobava123@gmail.com>
2024-10-30 20:31:58 +0400

    commit

---
+++ sript.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_etmm4kp","template_vq5u1cq",parms).then(alert("Email Sent!!!"))
+}
